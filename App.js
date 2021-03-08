63cbfa60695ae2bff43f904c18b58ae3a376c74f
Oh Kilho <prince@kilho.net>
2021-03-08 13:46:24 +0900

    Update

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView>
+        <Text style={styles.body}>Hello</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    fontSize: 24,
+  },
+});
+
+export default App;
